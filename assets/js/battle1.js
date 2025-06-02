db113038cadc17d6efaf84a765cf60e33b6d44a4
alice20040127 <66763324+alice20040127@users.noreply.github.com>
2025-06-03 07:02:28 +0800

    Update battle1.js

--- assets/js/battle1.js
+++ assets/js/battle1.js
@@ -226,7 +226,7 @@ gameArea.addEventListener('mouseup', (e) => {
 
 async function shoot(angle, power, dmg) {
     const rad = angle * Math.PI / 180;
-    const vx = power * powerScale * Math.cos(rad);
+    const vx = power * powerScale * Math.cos(rad)+20;
     const vy = power * powerScale * Math.sin(rad);
     let x = player.offsetLeft + 20;
     let y = 40;
