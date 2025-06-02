6e4c899a69da634b71e45c5747ff0e2e28fe72a3
alice20040127 <66763324+alice20040127@users.noreply.github.com>
2025-06-02 23:39:35 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,3 @@
-import { db } from './firebaseConfig.js';
-import {
-  ref,
-  get,
-  set,
-  child
-} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
-
 async function createCharacter() {
     const name = document.getElementById("playerName").value.trim();
     if (name === "") {
