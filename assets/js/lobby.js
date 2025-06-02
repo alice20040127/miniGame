7d9aa45625ba6c1723da6456ae4dbce325e646ed
alice20040127 <66763324+alice20040127@users.noreply.github.com>
2025-06-03 03:39:34 +0800

    Update lobby.js

--- assets/js/lobby.js
+++ assets/js/lobby.js
@@ -26,7 +26,7 @@ async function loadCharacterData() {
         const hardBtn = document.getElementById("hardBtn");
 
         playerNameOnPhoto.textContent = name; // 注意：這裡用的是 localStorage 中的 name
-        playerPhoto.src = "css/images/role1.jpg";
+        playerPhoto.src = "css/images/role-1.jpg";
 
         startGameBtn.addEventListener("click", () => {
             difficultyButtons.style.display = "block";
